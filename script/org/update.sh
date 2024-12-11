5a96b624229d6d1a040eb494384726e1cd0ad2f6
Ian Rudie <ian.rudie@solo.io>
2024-12-11 11:48:26 -0500

    kube-style org automation (#25)
    * init kube-style org config
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * update teams, refine org
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * begin working on the script which manages the org
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * define workflow to update org, dry run only
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * update.sh fix to prevent unintended file execution
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * add notify-call-to-vote team to org
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    ---------
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>

---
+++ script/org/update.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+REPO_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)
+readonly REPO_ROOT
+
+# these org admins are required for safety!
+# peribolos requires that the user it runs as be an org admin, also for safety
+readonly org_admins=(
+  ilevine
+  ilrudie
+  linsun
+  yuval-k
+)
+
+cd "${REPO_ROOT}"
+
+readonly BIN_PATH="${REPO_ROOT}/bin"
+
+cmd="${BIN_PATH}/peribolos"
+
+# remove any existing peribolos, always install to prevent execution of an unintended file 
+if [ -e "${cmd}" ]; then
+  rm "${cmd}"
+fi
+
+# install peribolos
+GOBIN=${BIN_PATH} go install sigs.k8s.io/prow/cmd/peribolos@main
+
+# maybe add --ignore-invitees
+args=(
+  --github-token-path="${REPO_ROOT}/GH_TOK"
+  --config-path="${REPO_ROOT}/org.yaml"
+  --fix-org
+  --fix-org-members
+  --fix-teams
+  --fix-team-members
+  --fix-team-repos
+  "${org_admins[@]/#/--required-admins=}"
+)
+
+# if you are feeling brave, run this script with org:admin and add the `--confirm`
+"${cmd}" "${args[@]}" "${@}"
